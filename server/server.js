985943b26a12251ce49396a8faa461d1d1dd4e3c
7777Satish <onlinegamespartner2@gmail.com>
2025-08-12 12:47:07 +0530

    Server Fix

--- server/server.js
+++ server/server.js
@@ -4,54 +4,94 @@ const server = new WebSocket.Server({port: 8080});
 
 allPlayers = {};
 
-
 server.on('connection', (socket)=>{
     var playerId = Math.random().toString(36).substring(2, 15);
-    allPlayers[playerId] = { id: playerId, x: 0, y: 0 };
+    allPlayers[playerId] = { id: playerId, x: Math.random() * 800, y: Math.random() * 600, socket: socket };
 
     socket.on('message', (message)=>{
         recentMessage = JSON.parse(message);
-        console.log(JSON.stringify(recentMessage))
+        // console.log(JSON.stringify(recentMessage))
 
         if(recentMessage.type == 'join_request'){
-            allPlayers[playerId] = { id: playerId, x: 0, y: 0, socket: socket };
+            // allPlayers[playerId] = { id: playerId, x: 0, y: 0, socket: socket };
             socket.send(JSON.stringify({type: 'player_joined', data: { id: playerId }}));
         }
 
         if(recentMessage.type == "find_match"){
             // Find a match for the player
-            const opponent = Object.values(allPlayers).find(player => player.id !== playerId);
+            allPlayers[playerId].isReady = true;
+            const opponent = Object.values(allPlayers).find(player => player.id !== playerId && !player.inAMatch && player.socket.readyState === WebSocket.OPEN && player.isReady);
             if (opponent) {
+                var role = Math.random() < 0.5 ? 'catcher' : 'runner';
                 allPlayers[playerId].opponent = opponent;
-                socket.send(JSON.stringify({ type: 'match_found'  }));
-                opponent.socket.send(JSON.stringify({ type: 'match_found' }));
+                allPlayers[playerId].inAMatch = true;
+                allPlayers[playerId].role = role;
+                opponent.opponent = allPlayers[playerId];
+                opponent.inAMatch = true;
+                opponent.role = role === 'catcher' ? 'runner' : 'catcher';
+                socket.send(JSON.stringify({ type: 'match_found', data: {role: role, oppId: opponent.id, pos: [allPlayers[playerId].x, allPlayers[playerId].y], oppPos: [opponent.x, opponent.y]} }));
+                opponent.socket.send(JSON.stringify({ type: 'match_found', data: {role: opponent.role, oppId: playerId, pos: [opponent.x, opponent.y], oppPos: [allPlayers[playerId].x, allPlayers[playerId].y]} }));
+
+                setTimeout(() => {
+                    if(!allPlayers[playerId] || !allPlayers[opponent.id] || !allPlayers[playerId].inAMatch || !allPlayers[opponent.id].inAMatch) return;
+                    allPlayers[playerId].isReady = false;
+                    opponent.isReady = false;
+                    allPlayers[playerId].inAMatch = false;
+                    opponent.inAMatch = false;
+
+                    var isPlayerWinner = (allPlayers[playerId].role === 'runner');
+                    socket.send(JSON.stringify({ type: 'game_over', data: {winner: isPlayerWinner ? playerId : opponent.id} }));
+                    opponent.socket.send(JSON.stringify({ type: 'game_over', data: {winner: isPlayerWinner ? playerId : opponent.id} }));
+
+                }, 20000);
             } else {
                 socket.send(JSON.stringify({ type: 'waiting_for_opponent' }));
             }
         }
 
         if(recentMessage.type == "move"){
-            console.log("Move event received", recentMessage.data);
+            if(!allPlayers[playerId].inAMatch) return;
+            // console.log("Move event received", recentMessage.data);
             const { dx, dy } = recentMessage.data;
             if (allPlayers[playerId]) {
                 allPlayers[playerId].x += dx;
-                allPlayers[playerId].y = dy;
+                allPlayers[playerId].y += dy;
             }
+
+            const opponent = allPlayers[playerId].opponent;
+            const player = allPlayers[playerId];
+
+            if(player.x < 0) player.x = 0;
+            if(player.y < 0) player.y = 0;
+
             
+            if(player.x + 50 > opponent.x && player.x < opponent.x + 50 && player.y + 50 > opponent.y && player.y < opponent.y + 50){
+                var isPlayerWinner = (allPlayers[playerId].role === 'catcher');
+                var data = {winner: isPlayerWinner ? playerId : opponent.id};
+                socket.send(JSON.stringify({ type: 'game_over',  data: data }));
+                opponent.socket.send(JSON.stringify({ type: 'game_over', data: data }));
+                player.inAMatch = false;
+                opponent.inAMatch = false;
+                player.isReady = false;
+                opponent.isReady = false;
+                return;
+            }
+
             data = {
                 players : {
                     [playerId]: {
-                        pos: [allPlayers[playerId].x, allPlayers[playerId].y],
-                        role: "catcher"
+                        pos: [player.x, player.y],
+                        role: [player.role]
                     },
-                    [allPlayers[playerId].opponent.id]: {
-                        pos: [allPlayers[allPlayers[playerId].opponent.id].x, allPlayers[allPlayers[playerId].opponent.id].y],
-                        role: "runner"
+                    [opponent.id]: {
+                        pos: [opponent.x, opponent.y],
+                        role: [opponent.role]
                     }
                 }
             }
 
             socket.send(JSON.stringify({ type: 'state_update',  data: data }));
+            opponent.socket.send(JSON.stringify({ type: 'state_update', data: data }));
 
         }
 
@@ -59,6 +99,12 @@ server.on('connection', (socket)=>{
 
     socket.on('close', (event)=>{
         console.log('Client disconnected', event);
+
+        if(allPlayers[playerId].inAMatch){
+            allPlayers[playerId].opponent.socket.send(JSON.stringify({ type: 'game_over', data: {} }));
+            allPlayers[playerId].opponent.inAMatch = false;
+        }
+
         delete allPlayers[playerId];
         server.clients.forEach(client => {
             if (client.readyState === WebSocket.OPEN) {
